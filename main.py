ea3ca1b172aaaf0528c4ec49932bf99909cdbe8a
MartinezJ10 <jl.mendoza@unah.hn>
2025-11-16 21:42:49 -0600

    project set up

---
+++ main.py
@@ -0,0 +1,16 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"project": "POKE QUEUE"}
+
+@app.get("/api/version")
+async def version():
+    return {"version": "0.0.0"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
